c921d5a05cde2357f5543474f7fb16333014b915
yusufmertbal <41000469+yusufmertbal@users.noreply.github.com>
2020-07-11 19:53:16 +0300

    Add project files.

---
+++ CETPlatform/Areas/Identity/Data/CETPlatformUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CETPlatform.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the CETPlatformUser class
+    public class CETPlatformUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+    }
+}
---
+++ CETPlatform/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CETPlatform.Areas.Identity.Data;
+using CETPlatform.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CETPlatform.Areas.Identity.IdentityHostingStartup))]
+namespace CETPlatform.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CETPlatformContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CETPlatformContextConnection")));
+
+                services.AddDefaultIdentity<CETPlatformUser>(options => {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                })
+                    .AddEntityFrameworkStores<CETPlatformContext>();
+            });
+        }
+    }
+}
